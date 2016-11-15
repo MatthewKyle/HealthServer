35ed5931ea73d0571e50541a9070d7a77060fd96
Matthew Kyle <Matt@docitt.com>
2016-11-15 14:20:11 -0800

    Added Tests and Adjust projects

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # HealthServer
 ASP.NET Middleware for health checks
+
+Please see /src/Examples/BasicHost for how to do a basic setup.
+
+
--- src/Examples/BasicHost/Startup.cs
+++ src/Examples/BasicHost/Startup.cs
@@ -21,11 +21,7 @@ namespace TestHost
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddHealthServer().AddHealthChecks(new List<IHealthStatusCheck>()
-            {
-                new DefaultHealthCheck(),
-                new DefaultHealthCheck((context)=> context.AddCheckState(new HealthCheckResult("LamdaCheck", false, new { state="bad things happened."})))
-            });
+            services.AddHealthServer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
