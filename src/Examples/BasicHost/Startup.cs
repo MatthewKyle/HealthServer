8aca8eda02603d99420d8b32597f1d4eadfaf809
Matthew Kyle <mkyle@docitt.com>
2016-11-15 16:26:02 -0800

    Fixed Configuration Options for Handlers

--- src/Examples/BasicHost/Startup.cs
+++ src/Examples/BasicHost/Startup.cs
@@ -5,17 +5,24 @@
 
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Logging;
 
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+        public Startup(IHostingEnvironment env)
         {
-            services.AddHealthServer();
+            var builder =
+                new ConfigurationBuilder().SetBasePath(env.ContentRootPath)
+                    .AddJsonFile("appsettings.json", true, true)
+                    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true)
+                    .AddEnvironmentVariables();
+
+            this.Configuration = builder.Build();
         }
+
+        public IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
@@ -26,7 +33,15 @@
             {
                 app.UseDeveloperExceptionPage();
             }
+
             app.UseHealthServer();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthServer(this.Configuration);
         }
     }
 }
--- src/HealthServer/Configuration/DependencyInjection/HealthServerExtensions.cs
+++ src/HealthServer/Configuration/DependencyInjection/HealthServerExtensions.cs
@@ -5,15 +5,11 @@
     using HealthServer.Configuration.DependencyInjection.BuilderExtensions;
     using HealthServer.Configuration.DependencyInjection.Options;
 
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class HealthServerExtensions
     {
-        public static IHealthServerBuilder AddHealthServerBuilder(this IServiceCollection services)
-        {
-            return new HealthServerBuilder(services);
-        }
-
         public static IHealthServerBuilder AddHealthServer(this IServiceCollection services)
         {
             var builder = services.AddHealthServerBuilder();
@@ -23,12 +19,27 @@
             return builder;
         }
 
-        public static IHealthServerBuilder AddHealthServer(this IServiceCollection services,
+        public static IHealthServerBuilder AddHealthServer(
+            this IServiceCollection services,
             Action<HealthServerHandlerOptions> setup)
         {
             services.Configure(setup);
 
             return services.AddHealthServer();
+        }
+
+        public static IHealthServerBuilder AddHealthServer(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<HealthServerHandlerOptions>(configuration.GetSection("HealthServerHandlerOptions"));
+
+            return services.AddHealthServer();
+        }
+
+        public static IHealthServerBuilder AddHealthServerBuilder(this IServiceCollection services)
+        {
+            return new HealthServerBuilder(services);
         }
     }
 }
